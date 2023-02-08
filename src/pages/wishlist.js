0ace45b2ea49a8506fd8512a2e162db480b657c5
Ayman Eldawy <aymaneldawy@yahoo.com>
2023-02-08 18:11:25 +0200

    import lang in wishlist page

--- src/pages/wishlist.js
+++ src/pages/wishlist.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Layout } from './../components/Layout/Layout';
 import { Breadcrumb } from './../components/Breadcrumb/Breadcrumb';
 import { SectionTitle } from './../components/Global/SectionTitle';
 import { TableProducts } from './../components/TableProducts/TableProducts';
 import { fetchWord } from '@/lang/fetchWord';
+import { LanguageContext } from './../context/LangContext';
 
 const Wishlist = () => {
+  const { lang } = useContext(LanguageContext);
   return (
     <Layout>
       <div className="mt-12">
