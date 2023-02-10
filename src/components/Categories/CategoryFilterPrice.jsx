2817cced76b6012fc4ea77f7b6908d7e619b5837
Ayman Eldawy <aymaneldawy@yahoo.com>
2023-02-10 22:02:13 +0200

    update slider price bar

--- src/components/Categories/CategoryFilterPrice.jsx
+++ src/components/Categories/CategoryFilterPrice.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import Slider from 'nouislider';
+import React, { useEffect } from 'react';
 
 import { PrimaryButton } from './../Global/PrimaryButton/PrimaryButton';
 
+import 'nouislider/dist/nouislider.css';
+
 export const CategoryFilterPrice = ({ title }) => {
+  useEffect(() => {
+    Slider.create(document.getElementById('sliderDouble'), {
+      start: [50, 260],
+      connect: [false, true, false],
+      step: 1,
+      range: { min: 0, max: 600 },
+    });
+  }, []);
+
   return (
-    <div className="mb-4 p-4 lg:px-8">
+    <div className="mb-4 p-4 lg:px-8 price-slide">
       <h3 className="text-primary mb-6 font-semibold text-xl">{title}</h3>
-      <div className="flex my-4">
-        <input
-          type="range"
-          className="appearance-none h-1 bg-[#D9D9D9] accent-primary w-full text-gray-400"
-        />
-        <input
-          type="range"
-          className="appearance-none h-1 bg-[#D9D9D9] accent-primary w-full text-gray-400"
-        />
+
+      <div className="input-slider-container">
+        <div className="slider input-slider--yellow" id="sliderDouble"></div>
       </div>
       <div className=" mt-4 flex items-center justify-between">
         <p className="flex gap-1 font-medium">
